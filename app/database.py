59acbb3824e182dc7d0cadc5e00bf2720634228f
ranra <arnarar292@nemi.tskoli.is>
2018-04-12 09:30:35 +0000

    utlit

--- app/database.py
+++ app/database.py
@@ -1,8 +1,23 @@
 import pymysql
-# conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='2209922929', passwd='mypassword', db='2209922929_vefur')
-# cur = conn.cursor()
+from app.routes import *
 
-def create(username, name, email, password):
+
+
+
+conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='2209922929', passwd='mypassword',db='2209922929_vefur')
+cur = conn.cursor()
+
+def get_img():
+    img_list=[]
+    cur.execute("SELECT * FROM images")
+    for row in cur:
+        img_list.append(row[2])
+    return img_list
+
+
+
+
+def create( username, name, email, password):
     cur.execute(
         "Insert into users values('{}','{}','{}','{}')".format(username, name, email, password))
     conn.commit()
@@ -11,9 +26,26 @@ def create(username, name, email, password):
 
 
 
-def check(self, username, password):
+def check():
     cur.execute("SELECT * FROM users")
-    for row,col in cur:
-        if username in row:
-            if password in col:
-                print("loggedin")
+    for row in cur:
+       print(row)
+
+
+
+
+
+def img_insert():
+    try:
+        with conn.cursor() as cursor:
+            for i in range(len(image_names)):
+                sql = "INSERT INTO `images` (`img_id`,`img_name`, `img_link`) VALUES (%s, %s, %s)"
+                cursor.execute(sql, (str(i), "mynd", image_names[i]))
+        conn.commit()
+    finally:
+        print("done")
+        conn.close()
+
+
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,17 @@
 from flask import Flask
 import os
-from flask import render_template, flash, redirect, url_for
-#from new_app.database import *
+from flask import render_template, flash, redirect, url_for, send_from_directory
+from app.database import *
 app = Flask(__name__)
-from new_app.config import Config
+from app.config import Config
 app.config.from_object(Config)
 
 img_folder = os.path.join("static", "img")
 
 from app.forms import LoginForm
 
+image_names = os.listdir('./img')
+list_length = len(image_names)
 @app.route('/')
 @app.route('/index')
 def index():
@@ -33,9 +35,24 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        #create(form.username.data, "arnar", " blabla", form.password.data)
+        create( form.username.data, "arnar", " blabla", form.password.data)
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+
+
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("img", filename)
+
+@app.route('/gallery')
+def get_gallery():
+    img_list = get_img()
+
+    #check()
+    #image_names = os.listdir('./img')
+    list_length= len(img_list)
+    return render_template("gallery.html", image_names=img_list, list_length = list_length)
 
 if __name__ == '__main__':
       app.run()
