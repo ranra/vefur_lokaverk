6fc78f6375e7315fe18163ea7a9115026d96fa6c
ranra <arnarar292@nemi.tskoli.is>
2018-04-17 08:48:45 +0000

    translation

--- vsh_lokaverk/app/__init__.py
+++ vsh_lokaverk/app/__init__.py
@@ -10,6 +10,8 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +21,7 @@ login = LoginManager(app)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 login.login_view = 'login'
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -48,3 +51,7 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 from app import routes, models, errors
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
