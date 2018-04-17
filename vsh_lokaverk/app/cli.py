762b3e40e066f4de26cc496a31badb4f66e37a5c
ranra <arnarar292@nemi.tskoli.is>
2018-04-17 09:42:12 +0000

    translation done

--- vsh_lokaverk/app/cli.py
+++ vsh_lokaverk/app/cli.py
@@ -21,3 +21,16 @@ def compile():
     """Compile all languages."""
     if os.system('pybabel compile -d app/translations'):
         raise RuntimeError('compile command failed')
+
+
+
+@translate.command()
+@click.argument('lang')
+def init(lang):
+    """Initialize a new language."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system(
+            'pybabel init -i messages.pot -d app/translations -l ' + lang):
+        raise RuntimeError('init command failed')
+    os.remove('messages.pot')
