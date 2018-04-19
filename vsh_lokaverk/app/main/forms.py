be6cedf4dce6c123392fccdeb213637b0c8add1a
ranra <arnarar292@nemi.tskoli.is>
2018-04-19 16:36:29 +0000

    search

--- vsh_lokaverk/app/main/forms.py
+++ vsh_lokaverk/app/main/forms.py
@@ -4,6 +4,7 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Length
 from flask_babel import _, lazy_gettext as _l
 from app.models import User
+from flask import request
 
 
 class EditProfileForm(FlaskForm):
